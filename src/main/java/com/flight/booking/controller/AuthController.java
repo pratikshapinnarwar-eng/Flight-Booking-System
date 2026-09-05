f954ccc9402fcc3dc674db9b737cd5bd09fbe3ef
Pratiksha Pinnarwar <pratikshamarotipinnarwar@gmail.com>
2026-09-05 12:02:13 +0530

    refactor: configure CORS with named origins from application config

--- src/main/java/com/flight/booking/controller/AuthController.java
+++ src/main/java/com/flight/booking/controller/AuthController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
-    @CrossOrigin(origins = "*")
+    @CrossOrigin(origins = {\"http://localhost:4200\", \"http://localhost:3000\"}))
 
     private final AuthService authService;
 
